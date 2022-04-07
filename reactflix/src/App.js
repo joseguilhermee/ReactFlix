8be742f55fca2f1924323ad895e03a27ae7bb7ca
Guilherme <joseguilhermeeneto@gmail.com>
2022-04-07 09:22:26 -0300

    Filme em destaque back

--- reactflix/src/App.js
+++ reactflix/src/App.js
@@ -3,18 +3,25 @@ import React, { useEffect, useState } from 'react';
 import './App.css'
 import Tmdb from './requestFilm';
 import MovieRow from './components/movieRow'
+import FeaturedMovie from './components/FeaturedMovie';
  
 export default () => {
 /*padrão comum para salvar o state*/
   const [movieList, setMovieList] = useState([])
+  const [featuredData, setfeaturedData] = useState(null)
   useEffect(()=>{
 
-
+    
+    
     const loadAll = async () =>{
       let list = await Tmdb.getHomeList();
       setMovieList(list);
-
-
+      
+      //pegando o Filme em Destaque
+      let originals = list.filter(i=>i.slug === 'originals')
+      let randomChosen = Math.floor(Math.random() * (originals[0].itens.results.length -1));
+      let chosen = originals[0].itens.results[randomChosen]
+      console.log(chosen)
     }
 
     loadAll();
@@ -25,6 +32,10 @@ export default () => {
 
   return (
     <div className="page">
+
+      {featuredData && 
+        <FeaturedMovie item={featuredData} />
+      }
       <section className="lists">
         
         {movieList.map((iten, key)=>(
